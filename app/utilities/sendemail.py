6459c3d6e8dea44ae7f6717dd2cd4c6cb016dd30
zaranabhalala <zaranabhalala1996@gmail.com>
2021-05-09 16:16:39 -0400

    Feature: Updated code for sending email

--- app/utilities/sendemail.py
+++ app/utilities/sendemail.py
@@ -10,7 +10,7 @@ def absolute_path(filepath):
 
 
 def sendemail(to):
-    port = 465  # For SSL
+    port = 587
     smtp_server = "smtp.gmail.com"
     sender_email = "zaranabhalala1996@gmail.com"  # Enter your address
     receiver_email = to  # Enter receiver address
@@ -20,6 +20,7 @@ def sendemail(to):
     This message is sent from MLB Player's Team"""
 
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
